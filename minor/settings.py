8e016c8f6273680fe038749bed8eeff9eac905b3
Rajendra Sarathe <143543512+rajendra123444@users.noreply.github.com>
2026-05-16 07:57:01 +0530

    p

--- minor/settings.py
+++ minor/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'
 # ✅ Correct way – no self‑import
@@ -16,6 +17,7 @@ ALLOWED_HOSTS = [
     'localhost', 
     '127.0.0.1', 
     'oasis-toolkit-evaluating-consultant.trycloudflare.com'
+    ,'e-learning-project-gray.vercel.app'
                  ]
 
 # Application definition
@@ -63,10 +65,11 @@ WSGI_APPLICATION = 'minor.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('POSTGRES_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Password validation
